92e5854f6a40842641f28205eccae7fbf873ab92
ilor <kailoran@gmail.com>
2010-05-28 16:13:04 +0200

    tagger app upgrade

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,14 +1,43 @@
 #include "morph/sfstanalyser.h"
 
 #include <iostream>
+#include <boost/algorithm/string.hpp>
+#include <boost/program_options.hpp>
 
-int main()
+#include "debug.h"
+
+int main(int argc, char** argv)
 {
-	PlTagger::SfstAnalyser fst("/home/ilor/semantic/tagger/fst/M.cfst");
+	std::string sfst;
+	using boost::program_options::value;
+	boost::program_options::options_description desc("Allowed options");
+	desc.add_options()
+			("sfst-transducer,t", value(&sfst)->default_value("/home/ilor/semantic/tagger/fst/M.cfst"),
+			 "SFST transducer file to use (compact format)")
+			("help,h", "Show help")
+			;
+	boost::program_options::variables_map vm;
+
+	try {
+		boost::program_options::store(boost::program_options::command_line_parser(argc, argv).options(desc).run(), vm);
+	} catch (boost::program_options::error& e) {
+		std::cerr << e.what() << "\n";
+		return 2;
+	}
+	boost::program_options::notify(vm);
+
+	if (vm.count("help")) {
+		std::cout << desc << "\n";
+		return 1;
+	}
+
+	PlTagger::SfstAnalyser fst(sfst);
 	while (std::cin.good()) {
 		std::string s;
 		std::cin >> s;
 		Toki::Token t(s.c_str(), "t", Toki::Whitespace::None);
 		PlTagger::Token* tt = fst.process(t);
+		std::cout << PlTagger::token_string(*tt) << "\n";
+		delete tt;
 	}
 }
