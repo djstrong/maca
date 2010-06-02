6025e4c1fea3cb60c90955eaa843648557ac2161
ilor <kailoran@gmail.com>
2010-06-02 02:12:28 +0200

    tab-to-stst.cpp stub

---
+++ tools/tab-to-sfst.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <unicode/unistr.h>
+
+int main(int argc, char** argv)
+{
+	if (argc < 3) {
+		std::cout << "Usage: tab-to-sfst infile outfile\n";
+		exit(1)
+	}
+	
+	
+}
