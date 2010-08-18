09727c58af888d7a8f6bef3304f9a3316598e2fd
ilor <ilor@ilor-laptop.(none)>
2010-08-18 11:24:43 +0200

    tagset-tool upgrade:  - loads named tagsets now  - tag parse mode for parsing complex tags  - code cleanups and error handling

--- tagset-tool/main.cpp
+++ tagset-tool/main.cpp
@@ -2,6 +2,7 @@
 #include <libpltagger/util/debug.h>
 #include <libpltagger/util/settings.h>
 #include <libpltagger/tagsetparser.h>
+#include <libpltagger/tagsetmanager.h>
 
 #include <libtoki/util/foreach.h>
 
@@ -9,10 +10,106 @@
 
 #include <fstream>
 
+void tagset_info(const PlTagger::Tagset& tagset)
+{
+	std::cerr << "Tagset loaded: "
+		<< tagset.pos_dictionary().size() << " POSes, "
+		<< tagset.attribute_dictionary().size() << " attributes, "
+		<< tagset.value_dictionary().size() << " values\n";
+	std::cerr << "Size is " << tagset.size()
+		<< " (extra size is " << tagset.size_extra() << ")\n";
+	std::cerr << "POSes: ";
+	foreach (const std::string& s, tagset.pos_dictionary()) {
+		std::cerr << s << " ";
+	}
+	std::cerr << "\n";
+}
+
+void tagset_query_loop(const PlTagger::Tagset& tagset)
+{
+	while (std::cin.good()) {
+		std::string s;
+		std::cin >> s;
+		if (!s.empty()) {
+			PlTagger::pos_idx_t pos = tagset.pos_dictionary().get_id(s);
+			PlTagger::attribute_idx_t atr = tagset.attribute_dictionary().get_id(s);
+			PlTagger::value_idx_t val = tagset.value_dictionary().get_id(s);
+			if (tagset.pos_dictionary().is_id_valid(pos)) {
+				std::cout << s << " -> POS ->" ;
+				foreach (PlTagger::attribute_idx_t a, tagset.get_pos_attributes(pos)) {
+					std::string astr = tagset.attribute_dictionary().get_string(a);
+					if (tagset.get_pos_required_attributes(pos)[a]) {
+						std::cout << " " << astr;
+					} else {
+						std::cout << " [" << astr << "]";
+					}
+				}
+				std::cout << "\n";
+			} else if (tagset.attribute_dictionary().is_id_valid(atr)) {
+				std::cout << s << " -> attribute ->";
+				foreach (PlTagger::value_idx_t v, tagset.get_attribute_values(atr)) {
+					std::cout << " " << tagset.value_dictionary().get_string(v);
+				}
+				std::cout << "\nIn POSes:";
+				for (PlTagger::pos_idx_t p = (PlTagger::pos_idx_t)(0); p < tagset.pos_dictionary().size(); ++p) {
+					if (tagset.get_pos_valid_attributes(p)[atr]) {
+						std::cout << " " << tagset.pos_dictionary().get_string(p);
+						if (!tagset.get_pos_required_attributes(p)[atr]) {
+							std::cout << "?";
+						}
+					}
+				}
+				std::cout << "\n";
+			} else if (tagset.value_dictionary().is_id_valid(val)) {
+				PlTagger::attribute_idx_t a = tagset.get_value_attribute(val);
+				std::cout << s << " -> value -> attribute ";
+				std::cout << tagset.attribute_dictionary().get_string(a);
+				std::cout << " .";
+				foreach (PlTagger::value_idx_t v, tagset.get_attribute_values(a)) {
+					std::cout << " " << tagset.value_dictionary().get_string(v);
+				}
+				std::cout << "\nIn POSes:";
+				for (PlTagger::pos_idx_t p = (PlTagger::pos_idx_t)(0); p < tagset.pos_dictionary().size(); ++p) {
+					if (tagset.get_pos_valid_attributes(p)[a]) {
+						std::cout << " " << tagset.pos_dictionary().get_string(p);
+						if (!tagset.get_pos_required_attributes(p)[a]) {
+							std::cout << "?";
+						}
+					}
+				}
+				std::cout << "\n";
+			} else {
+				std::cout << "Not recognized: " << s << "\n";
+			}
+		}
+	}
+}
+
+void tag_parse_loop(const PlTagger::Tagset& tagset, bool validate)
+{
+	while (std::cin.good()) {
+		std::string s;
+		std::cin >> s;
+		try {
+			std::vector<PlTagger::Tag> tags = tagset.parse_tag(s, true);
+			foreach (const PlTagger::Tag& tag, tags) {
+				std::cout << tagset.tag_to_string(tag);
+				if (validate) {
+					tagset.validate_tag(tag, false, &std::cout);
+				}
+				std::cout << "\n";
+			}
+		} catch (PlTagger::TagParseError& e) {
+			std::cerr << "TagParseError: " << e.info() << "\n";
+		}
+	}
+}
+
 int main(int argc, char** argv)
 {
 	std::string tagset_load, tagset_save;
 	bool quiet = false;
+	bool parse = false, validate = false;
 	using boost::program_options::value;
 
 	boost::program_options::options_description desc("Allowed options");
@@ -23,6 +120,10 @@ int main(int argc, char** argv)
 			 "Path to tagset ini file to save\n")
 			("quiet,q", value(&quiet)->zero_tokens(),
 			 "Suppress startup info when loading a tagset\n")
+			("parse,p", value(&parse)->zero_tokens(),
+			 "Parse complex tag strings mode")
+			("validate,v", value(&validate)->zero_tokens(),
+			 "Validate parsed tags")
 			("help,h", "Show help")
 			;
 	boost::program_options::variables_map vm;
@@ -44,28 +145,12 @@ int main(int argc, char** argv)
 		return 1;
 	}
 
-	boost::shared_ptr<PlTagger::Tagset> tagset(new PlTagger::Tagset);
 	if (!tagset_load.empty()) {
-		std::ifstream ifs(tagset_load.c_str());
-		if (ifs.good()) {
-			try {
-				*tagset = PlTagger::TagsetParser::load_ini(ifs);
-			} catch (PlTagger::TagsetParseError& e) {
-				std::cerr << e.info() << "\n";
-				exit(1);
-			}
+		try {
+			const PlTagger::Tagset& tagset = PlTagger::get_named_tagset(tagset_load);
+
 			if (!quiet) {
-				std::cerr << "Tagset loaded: "
-					<< tagset->pos_dictionary().size() << " POSes, "
-					<< tagset->attribute_dictionary().size() << " attributes, "
-					<< tagset->value_dictionary().size() << " values\n";
-				std::cerr << "Size is " << tagset->size()
-					<< " (extra size is " << tagset->size_extra() << ")\n";
-				std::cerr << "POSes: ";
-				foreach (const std::string& s, tagset->pos_dictionary()) {
-					std::cerr << s << " ";
-				}
-				std::cerr << "\n";
+				tagset_info(tagset);
 			}
 
 			if (!tagset_save.empty()) {
@@ -73,71 +158,22 @@ int main(int argc, char** argv)
 				if (!quiet) {
 					std::cerr << "Saving tagset to " << tagset_save << " ... ";
 				}
-				PlTagger::TagsetParser::save_ini(*tagset, ofs);
+				PlTagger::TagsetParser::save_ini(tagset, ofs);
 				if (!quiet) {
 					std::cerr << "ok\n";
 				}
 			}
 
-			while (std::cin.good()) {
-				std::string s;
-				std::cin >> s;
-				if (!s.empty()) {
-					PlTagger::pos_idx_t pos = tagset->pos_dictionary().get_id(s);
-					PlTagger::attribute_idx_t atr = tagset->attribute_dictionary().get_id(s);
-					PlTagger::value_idx_t val = tagset->value_dictionary().get_id(s);
-					if (tagset->pos_dictionary().is_id_valid(pos)) {
-						std::cout << s << " -> POS ->" ;
-						foreach (PlTagger::attribute_idx_t a, tagset->get_pos_attributes(pos)) {
-							std::string astr = tagset->attribute_dictionary().get_string(a);
-							if (tagset->get_pos_required_attributes(pos)[a]) {
-								std::cout << " " << astr;
-							} else {
-								std::cout << " [" << astr << "]";
-							}
-						}
-						std::cout << "\n";
-					} else if (tagset->attribute_dictionary().is_id_valid(atr)) {
-						std::cout << s << " -> attribute ->";
-						foreach (PlTagger::value_idx_t v, tagset->get_attribute_values(atr)) {
-							std::cout << " " << tagset->value_dictionary().get_string(v);
-						}
-						std::cout << "\nIn POSes:";
-						for (PlTagger::pos_idx_t p = (PlTagger::pos_idx_t)(0); p < tagset->pos_dictionary().size(); ++p) {
-							if (tagset->get_pos_valid_attributes(p)[atr]) {
-								std::cout << " " << tagset->pos_dictionary().get_string(p);
-								if (!tagset->get_pos_required_attributes(p)[atr]) {
-									std::cout << "?";
-								}
-							}
-						}
-						std::cout << "\n";
-					} else if (tagset->value_dictionary().is_id_valid(val)) {
-						PlTagger::attribute_idx_t a = tagset->get_value_attribute(val);
-						std::cout << s << " -> value -> attribute ";
-						std::cout << tagset->attribute_dictionary().get_string(a);
-						std::cout << " ->";
-						foreach (PlTagger::value_idx_t v, tagset->get_attribute_values(a)) {
-							std::cout << " " << tagset->value_dictionary().get_string(v);
-						}
-						std::cout << "\nIn POSes:";
-						for (PlTagger::pos_idx_t p = (PlTagger::pos_idx_t)(0); p < tagset->pos_dictionary().size(); ++p) {
-							if (tagset->get_pos_valid_attributes(p)[a]) {
-								std::cout << " " << tagset->pos_dictionary().get_string(p);
-								if (!tagset->get_pos_required_attributes(p)[a]) {
-									std::cout << "?";
-								}
-							}
-						}
-						std::cout << "\n";
-					} else {
-						std::cout << "Not recognized: " << s << "\n";
-					}
-				}
+			if (parse) {
+				std::cerr << "(Tag parse mode)\n";
+				tag_parse_loop(tagset, validate);
+			} else {
+				std::cerr << "(Tagset query mode)\n";
+				tagset_query_loop(tagset);
 			}
-		} else {
-			std::cerr << "tagset file open error\n";
-			return 7;
+		} catch (PlTagger::PlTaggerError& e) {
+			std::cerr << "PlTagger Error: " << e.info() << "\n";
+			return 4;
 		}
 	} else {
 		std::cerr << "Usage: tagset-tool [OPTIONS] <tagset-file>\n";
