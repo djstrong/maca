ed8da7c2ab7e98f120fb4a11c3e5e1f572cfbf69
ilor <kailoran@gmail.com>
2010-08-25 15:38:16 +0200

    update api-example

--- doc/api-example.cpp
+++ doc/api-example.cpp
@@ -5,12 +5,14 @@
 #include <libpltagger/morph/dispatchanalyser.h>
 #include <libpltagger/util/settings.h>
 
+#include <boost/scoped_ptr.hpp>
+
 #include <iostream>
 
 int main(int argc, char** argv)
 {
 	std::string config;
-	if(argc > 1) config = argv[1];
+	if (argc > 1) config = argv[1];
 	else config = "morfeusz";	
 	PlTagger::Config::Node cfg = PlTagger::get_named_config(config);
 	
@@ -20,21 +22,19 @@ int main(int argc, char** argv)
 	PlTagger::DispatchAnalyser analyser(cfg);
 	
 	while (sentr.has_more()) {
-			std::vector<Toki::Token*> sentence = sentr.get_next_sentence();
-			assert(!sentence.empty());
-			std::vector<PlTagger::Token*> analysed_sentence = analyser.process_dispose(sentence);
-			std::cout << "{\n";
-			foreach (PlTagger::Token *tok, analysed_sentence)
+		boost::scoped_ptr<Toki::Sentence> sentence(sentr.get_next_sentence());
+		assert(!sentence->empty());
+		boost::scoped_ptr<PlTagger::Sentence> analysed_sentence(analyser.process_dispose(sentence.get()));
+		std::cout << "{\n";
+		foreach (PlTagger::Token *tok, analysed_sentence->tokens())
+		{
+			std::cout << "\t" << tok->orth_utf8();
+			foreach(const PlTagger::Lexeme &lex, tok->lexemes())
 			{
-				std::cout << "\t" << tok->orth_utf8();
-				foreach(const PlTagger::Lexeme &lex, tok->lexemes())
-				{
-					std::cout << "\n\t\t" << lex.lemma_utf8();
-				}
-				std::cout << "\n";
-				delete tok;
-			};
-			std::cout << "}\n";
-		}
-	return 0;
+				std::cout << "\n\t\t" << lex.lemma_utf8();
+			}
+			std::cout << "\n";
+		};
+		std::cout << "}\n";
+	}
 }
