cbe1d875320addc11aaa09fd6ebfdd668e697935
ilor <kailoran@gmail.com>
2010-09-10 14:07:03 +0200

    add SentenceAnalyser

---
+++ libmaca/util/sentenceanalyser.cpp
@@ -0,0 +1,73 @@
+#include <libmaca/util/sentenceanalyser.h>
+#include <libmaca/util/settings.h>
+#include <libtoki/tokenizer/layertokenizer.h>
+#include <libmaca/morph/dispatchanalyser.h>
+#include <boost/scoped_ptr.hpp>
+#include <boost/make_shared.hpp>
+
+namespace Maca {
+
+	SentenceAnalyser::SentenceAnalyser(boost::shared_ptr<Toki::Tokenizer> tok,
+			boost::shared_ptr<MorphAnalyser> ma)
+	: UnicodeSink(), tok_(tok), sp_(*tok_), ma_(ma)
+	{
+	}
+
+	SentenceAnalyser::SentenceAnalyser(const Config::Node &cfg)
+		: UnicodeSink()
+		, tok_(Toki::LayerTokenizer::create_from_named_config(cfg.get("general.toki-config", "")))
+		, sp_(*tok_)
+		, ma_(new DispatchAnalyser(cfg))
+	{
+	}
+
+	SentenceAnalyser::SentenceAnalyser(const Config::Node &cfg, const Toki::Config::Node &toki_config_override)
+		: UnicodeSink()
+		, tok_(new Toki::LayerTokenizer(toki_config_override))
+		, sp_(*tok_)
+		, ma_(new DispatchAnalyser(cfg))
+	{
+	}
+
+	boost::shared_ptr<SentenceAnalyser> SentenceAnalyser::create_from_named_config(std::string config_name)
+	{
+		const Config::Node& cfg = get_named_config(config_name);
+		return boost::make_shared<SentenceAnalyser>(cfg);
+	}
+
+	boost::shared_ptr<SentenceAnalyser> SentenceAnalyser::create_from_named_config(std::string config_name, std::string toki_config_override)
+	{
+		const Config::Node& cfg = get_named_config(config_name);
+		const Toki::Config::Node& toki_cfg = Toki::get_named_config(toki_config_override);
+		return boost::make_shared<SentenceAnalyser>(cfg, toki_cfg);
+	}
+
+	Sentence* SentenceAnalyser::get_next_sentence()
+	{
+		if (sp_.has_more()) {
+			boost::scoped_ptr<Toki::Sentence> toki_sentence(sp_.get_next_sentence());
+			assert(toki_sentence);
+			assert(!toki_sentence->empty());
+			return ma_->process(*toki_sentence);
+		} else {
+			return NULL;
+		}
+	}
+
+	void SentenceAnalyser::new_input_source()
+	{
+		tok_->reset();
+		tok_->set_input_source(get_input_source());
+	}
+
+	bool SentenceAnalyser::process(sentence_sink_t sink)
+	{
+		bool had_sentences = false;
+		while (Sentence* s = get_next_sentence()) {
+			sink(s);
+			had_sentences = true;
+		}
+		return had_sentences;
+	}
+
+} /* end ns Maca */
---
+++ libmaca/util/sentenceanalyser.h
@@ -0,0 +1,64 @@
+#ifndef LIBMACA_UTIL_SENTENCEANALYSER_H
+#define LIBMACA_UTIL_SENTENCEANALYSER_H
+
+#include <libtoki/tokenizer/tokenizer.h>
+#include <libtoki/sentencesplitter.h>
+#include <libmaca/morph/morphanalyser.h>
+#include <libmaca/util/confignode.h>
+
+namespace Maca {
+
+	class SentenceAnalyser : public Toki::UnicodeSink
+	{
+	public:
+		SentenceAnalyser(boost::shared_ptr<Toki::Tokenizer> tok,
+			boost::shared_ptr<MorphAnalyser> ma);
+
+		SentenceAnalyser(const Config::Node& cfg);
+
+		SentenceAnalyser(const Config::Node& cfg, const Toki::Config::Node& toki_config_override);
+
+		static boost::shared_ptr<SentenceAnalyser> create_from_named_config(const std::string config_name);
+
+		static boost::shared_ptr<SentenceAnalyser> create_from_named_config(const std::string config_name, const std::string toki_config_override);
+
+		Sentence* get_next_sentence();
+
+		typedef boost::function<void (Sentence*)> sentence_sink_t;
+
+		/**
+		 * Convenience function to process all sentences that can be gathered
+		 * and feed them one by one to the sink function.
+		 * @return true if the sink was called at least once, false otherwise
+		 *         (meaning there were no sentences)
+		 */
+		bool process(sentence_sink_t sink);
+
+		const Toki::Tokenizer& tokenizer() const {
+			return *tok_;
+		}
+
+		const MorphAnalyser& analyser() const {
+			return *ma_;
+		}
+
+		const Tagset& tagset() const {
+			return ma_->tagset();
+		}
+
+	protected:
+		void new_input_source();
+
+	private:
+		boost::shared_ptr<Toki::Tokenizer> tok_;
+
+		Toki::SentenceSplitter sp_;
+
+		boost::shared_ptr<MorphAnalyser> ma_;
+	};
+
+
+
+} /* end ns Maca */
+
+#endif // LIBMACA_UTIL_SENTENCEANALYSER_H
