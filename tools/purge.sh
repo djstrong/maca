da8dc9ddf6a38d9004a8b51fdd756e30ea9be703
ilor <kailoran@gmail.com>
2010-09-22 11:28:27 +0200

    change data dir, update purge script

--- tools/purge.sh
+++ tools/purge.sh
@@ -3,5 +3,6 @@ for i in toki maca corpus2 pwrutils; do
 	rm -rf /usr/local/include/lib$i
 	rm -rf /usr/local/lib/lib$i*
 	rm -rf /usr/local/lib/$i/
-	rm -rf /usr/share/lib$i
+	rm -rf /usr/local/share/lib$i
+	rm -rf /usr/local/share/$i
 done
