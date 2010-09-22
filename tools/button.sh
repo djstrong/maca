0eb4d7206e245c0977533ca72ec0c8ba04417e22
ilor <kailoran@gmail.com>
2010-09-22 16:14:23 +0200

    update button script with options, pull and clean / rm cmakecache support

--- tools/button.sh
+++ tools/button.sh
@@ -12,14 +12,68 @@ function fail() {
 	exit 1
 }
 
+usage()
+{
+cat << EOF
+usage: $0 options
+
+OPTIONS:
+   -h      Show this message
+   -c      make clean before building
+   -e      rm CMakeCache before building
+   -n      skip make install step
+   -p      pull from git repo first
+EOF
+}
+
+OPT_CLEAN=
+OPT_CACHE=
+OPT_INSTALL=1
+OPT_PULL=
+while getopts "hcenp" OPTION
+do
+     case $OPTION in
+         h)
+             usage
+             exit 0
+             ;;
+         c)
+             OPT_CLEAN=1
+             ;;
+         e)
+             OPT_CACHE=1
+             ;;
+         n)
+             OPT_INSTALL=
+             ;;
+         p)
+             OPT_PULL=1
+             ;;
+     esac
+done
+
 for i in $PROJECTS; do
 	echo
-	echo "--- Building $i..."
+	if [[ ! -z $OPT_PULL ]]; then
+		echo "--- Pulling $i..."
+		git pull
+	fi
+	echo "--- Configuring $i..."
 	cd $BASEDIR/$i/$BUILDDIR || fail  "cd error"
+	if [[ ! -z $OPT_CACHE ]]; then
+		rm CMakeCache.txt
+	fi
 	cmake .. > /dev/null || fail "cmake error"
+	if [[ ! -z $OPT_CLEAN ]]; then
+		echo "--- Cleaning $i..."
+		make clean > /dev/null || fail "make clean error"
+	fi
+	echo "--- Building $i..."
 	make -j4 > /dev/null || fail  "make error"
-	echo "--- Installing $i..."
-	sudo make install > /dev/null || fail "make install error"
+	if [[ ! -z $OPT_INSTALL ]]; then
+		echo "--- Installing $i..."
+		sudo make install > /dev/null || fail "make install error"
+	fi
 	echo "--- Testing $i..."
 	make test > /dev/null || fail "make test error in"
 done
