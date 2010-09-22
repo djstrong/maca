51681ca2f49c9d11706637711fac509735376959
ilor <kailoran@gmail.com>
2010-09-22 14:20:22 +0200

    button scrupt to build corpus2, toki and maca

---
+++ tools/button.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+BUILDDIR=qtcreator-build
+PROJECTS="corpus2 toki maca"
+BASEDIR=../..
+CURDIR=`pwd`
+FILTER="grep -i -C 4 error"
+#FILTER=cat
+
+function fail() {
+	echo "!!! $1 in $i !!!"
+	cd $CURDIR
+	exit 1
+}
+
+for i in $PROJECTS; do
+	echo "Building $i..."
+	cd $BASEDIR/$i/$BUILDDIR || fail  "cd error"
+	cmake .. > /dev/null || fail "cmake error"
+	make -j4 > /dev/null || fail  "make error"
+	sudo make install > /dev/null || fail "make install error"
+	make test > /dev/null || fail "make test error in"
+done
+cd $CURDIR
