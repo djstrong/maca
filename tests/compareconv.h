e1c60f793186d77358963657fee982247e880603
ilor <kailoran@gmail.com>
2010-09-22 14:20:04 +0200

    add a compare-in-out file-based test suite for converters ready for expansion

---
+++ tests/compareconv.h
@@ -0,0 +1,8 @@
+#ifndef LIBMACA_COMPARECONV_H
+#define LIBMACA_COMPARECONV_H
+
+#include <boost/test/unit_test.hpp>
+
+void init_compareconv_suite(boost::unit_test::test_suite* ts, const std::string& path);
+
+#endif // LIBMACA_COMPARECONV_H
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,5 +1,6 @@
-#define BOOST_TEST_MODULE master
+//#define BOOST_TEST_MODULE master
 #include <boost/test/included/unit_test.hpp>
+#include "compareconv.h"
 
 BOOST_AUTO_TEST_CASE(test_test)
 {
@@ -7,3 +8,19 @@ BOOST_AUTO_TEST_CASE(test_test)
 	BOOST_CHECK(a == 0);
 }
 
+boost::unit_test::test_suite* init_unit_test_suite(int argc, char* argv[])
+{
+	boost::unit_test::test_suite* ts1 = BOOST_TEST_SUITE("compare");
+	std::string compare_path;
+	for (int i = 0; i < argc; ++i) {
+		if (strcmp(argv[i], "--compare-tests-dir") == 0) {
+			++i;
+			if (i < argc) {
+				compare_path = argv[i];
+			}
+		}
+	}
+	init_compareconv_suite(ts1, compare_path);
+	boost::unit_test::framework::master_test_suite().add(ts1);
+	return 0;
+}
